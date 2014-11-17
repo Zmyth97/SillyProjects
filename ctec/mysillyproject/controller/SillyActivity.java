423b357979370cd9fc80e38db1bf69df9b3b24bf
Zack Moss <zmyth97@gmail.com>
2014-11-17 13:05:00 -0700

    11/17/14
    Up to date

--- ctec/mysillyproject/controller/SillyActivity.java
+++ ctec/mysillyproject/controller/SillyActivity.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -42,8 +48,8 @@ public class SillyActivity extends Activity implements View.OnClickListener
 		//List of Colors in the ArrayList
 		colorList = new ArrayList<Integer>();
 		colorList.add(R.color.Red);
-		colorList.add(R.color.White);
 		colorList.add(R.color.Black);
+		colorList.add(R.color.White);
 		colorList.add(R.color.Blue);
 		colorList.add(R.color.Gray);
 		colorList.add(R.color.Lime);
@@ -74,6 +80,36 @@ public class SillyActivity extends Activity implements View.OnClickListener
 		switch (v.getId()){
 		case R.id.firstButton:
 			appLayout.setBackgroundResource(colorList.get(myRandom.nextInt(colorList.size() - 1)));
+			
+			NotificationCompat.Builder mBuilder =
+			        new NotificationCompat.Builder(this)
+			        .setSmallIcon(R.drawable.ic_launcher)
+			        .setContentTitle("You are Sexy Notifier")
+			        .setContentText("You have changed a Color!!!");
+			// Creates an explicit intent for an Activity in your app
+			Intent resultIntent = new Intent(this, SillyActivity.class);
+
+			// The stack builder object will contain an artificial back stack for the
+			// started Activity.
+			// This ensures that navigating backward from the Activity leads out of
+			// your application to the Home screen.
+			TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+			// Adds the back stack for the Intent (but not the Intent itself)
+			stackBuilder.addParentStack(SillyActivity.class);
+			// Adds the Intent that starts the Activity to the top of the stack
+			stackBuilder.addNextIntent(resultIntent);
+			PendingIntent resultPendingIntent =
+			        stackBuilder.getPendingIntent(
+			            0,
+			            PendingIntent.FLAG_UPDATE_CURRENT
+			        );
+			mBuilder.setContentIntent(resultPendingIntent);
+			NotificationManager mNotificationManager =
+			    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			// mId allows you to update the notification later on.
+			mNotificationManager.notify(1, mBuilder.build());
+			
+			
 			break;
 		case R.id.title:
 			appText.setTextColor(colorList.get(myRandom.nextInt(colorList.size() - 1)));
